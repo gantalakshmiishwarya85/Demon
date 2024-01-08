539bb1591d0da430f49edad63567700e57b3ef8a
gantalakshmiishwarya85 <135019861+gantalakshmiishwarya85@users.noreply.github.com>
2024-01-08 14:34:19 +0530

    Create Demon.py

---
+++ Demon.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.set_page_config(page_title='Demon Slayer')
+st.header("Zenitsu Character From Demon Slayer")
+col1,col2,col3,col4=st.columns(4)
+with col1:
+  st.subheader("善逸")
+  st.image("https://rukminim2.flixcart.com/image/850/1000/kw2fki80/poster/e/9/j/medium-demon-slayer-kimetsu-no-yaiba-zenitsu-agatsuma-anime-boys-original-imag8trahkqgfu93.jpeg?q=90",caption="Zenitsu",width=100,use_column_width=True)
+  st.write(" Zenitsu as Thunder ")
+with col2:
+  st.subheader("善逸が炭治郎と一緒にいるのが可愛い")
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgl_aMN4vzrbWqraHaGLF43cIn5CYyn47Nxg&usqp=CAU",caption="Zenitsu is being cute with Tanjiro",width=100,use_column_width=True)
+  st.write("Zenitsu is being cute with Tanjiro")
+with col3:
+  st.subheader("善逸は剣を取った")
+  st.image("https://i.pinimg.com/originals/cc/ed/5a/cced5a055436f9f0ed1ef11160ac2318.gif")
+  st.write("Zenitsu took the sword") 
+with col4:
+  st.subheader("善逸とアッパームーン(スパイダー)の戦闘シーン")
+  st.image("https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/a/a6/Zenitsu_vs_Son_Spider_Demon.gif/revision/latest?cb=20200120085338")
+  st.write("Zenitsu fight scene with Upper Moon(Spider)") 
