fb5a7cc7e75415757eeab28d4836764bf8a99e51
gantalakshmiishwarya85 <135019861+gantalakshmiishwarya85@users.noreply.github.com>
2024-01-08 23:35:12 +0530

    Update Demon.py

--- Demon.py
+++ Demon.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='Demon Slayer')
 st.header("Zenitsu Character From Demon Slayer")
-col1,col2,col3,col4=st.columns(4)
+col1,col2,col3,col4,col5,col6=st.columns(6)
 with col1:
   st.subheader("善逸")
   st.image("https://rukminim2.flixcart.com/image/850/1000/kw2fki80/poster/e/9/j/medium-demon-slayer-kimetsu-no-yaiba-zenitsu-agatsuma-anime-boys-original-imag8trahkqgfu93.jpeg?q=90",width=100,use_column_width=True)
@@ -11,10 +11,19 @@ with col2:
   st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgl_aMN4vzrbWqraHaGLF43cIn5CYyn47Nxg&usqp=CAU",width=200,use_column_width=True)
   st.write("Zenitsu is being cute with Tanjiro")
 with col3:
+  st.subheader("善逸は友達や禰豆子と楽しむ")
+  st.image("https://i.pinimg.com/originals/3a/97/1f/3a971f70595fad1df18045eea30057f5.gif")
+  st.image("https://gifdb.com/images/thumbnail/zenitsu-nezuko-tanjiro-inosuke-mma35ewyb81os1lx.gif")
+  st.write("Zenitsu fun with friends and Nezuko")
+with col4:
   st.subheader("善逸は剣を取った")
   st.image("https://i.pinimg.com/originals/cc/ed/5a/cced5a055436f9f0ed1ef11160ac2318.gif",width=200,use_column_width=True)
   st.write("Zenitsu took the sword") 
-with col4:
+with col5:
   st.subheader("善逸とアッパームーン(スパイダー)の戦闘シーン")
   st.image("https://qph.cf2.quoracdn.net/main-qimg-2040d2ebdcd42609d9e709c6a62737d0")
   st.write("Zenitsu fight scene with Upper Moon(Spider)") 
+with col6:
+  st.subheader("善逸と舌鬼の戦闘シーン")
+  st.image("https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/9/9b/Zenitsu_using_Thunderclap_and_Flash_on_Tongue_Demon.gif/revision/latest/scale-to-width/360?cb=20190622172836 ")
+  st.write("Zenitsu fight scene with Tongue Demon") 
